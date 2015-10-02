6eeb0ac240990b9ffab0cba3d0b8fa6185d81957
Rasmus Kivipõld <sizen@greeny.cs.tlu.ee>
2015-10-02 10:58:58 +0300

    tegin 5 tunni

---
+++ asd.php
@@ -0,0 +1,14 @@
+<?php	
+	//kõik AB'iga seonduv
+	
+	function welcomeUser($first_name){
+		echo "Tere ".$first_name;
+	}
+	
+	$first_name = "juku";
+	welcomeUse($first_name);
+	
+	$first_name = "juhan";
+	welcomeUse($first_name);
+	
+?>
