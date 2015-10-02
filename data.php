44c089f2688746afa0f41b31646f627a813b62a4
Rasmus Kivipõld <sizen@greeny.cs.tlu.ee>
2015-10-02 12:59:48 +0300

    added logout

--- data.php
+++ data.php
@@ -5,6 +5,14 @@
 		//suunan  login lehele
 		header("Location: login.php");
 	}
-
+	//login välja. aadressireal on ?logout=1
+	if(isset($_GET["logout"])){
+		//kustutab kõik sessiooni muutujad
+		session_destroy();
+		header("Location: login.php");
+	}
 ?>
-data
+<p>
+	Tere, <?=$_SESSION["user_email"]; ?>
+	<a href="?logout=1">Logi välja</a>
+</p>
